fdc225410f666fe1f2ac2e4677d32e7f6e76798d
Roman Bulinski <aboandrus@gmail.com>
2019-06-18 07:43:45 +0200

    Implemented test

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.math.BigInteger;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestMain {
@@ -10,19 +12,31 @@ public class TestMain {
         assertEquals(3, main.countArgs(1,2,3));
     }
 
-    
+    @Test
+    public void test02(){
+        Main main = new Main();
+        assertEquals(3, main.countArgs(1, 2, "uhsaf uas"));
+    }
+
+    @Test
+    public void test03(){
+        Main main = new Main();
+        assertEquals(4, main.countArgs('a', 865, "asfhgajsf", new BigInteger("123456")));
+    }
+
+    @Test
+    public void test04(){
+        Main main = new Main();
+        assertEquals(0, main.countArgs());
+    }
+
+    @Test
+    public void test05(){
+        Main main = new Main();
+        assertEquals(1, main.countArgs(1));
+    }
 
 
 
-//    public class SolutionTest {
-//        @Test
-//        public void sampleTest() {
-//            assertEquals(3, Arguments.countArgs(1, 2, 3));
-//            assertEquals(3, Arguments.countArgs(1, 2, "uhsaf uas"));
-//            assertEquals(1, Arguments.countArgs(1));
-//            assertEquals(4, Arguments.countArgs('a', 865, "asfhgajsf", new BigInteger("123456")));
-//            assertEquals(0, Arguments.countArgs());
-//        }
-//    }
 
 }
